5eb4c5803cfce4416d6e99fedd31d4510b9cee63
sonak17 <101388325+sonak17@users.noreply.github.com>
2022-12-13 15:11:20 +0530

    Update views.py
    asas

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello guys welcome to azure fundamentals")
+    return HttpResponse("Hello guys welcome to azure fundamental training")
